8b2477027c9105c97123c153b5353961201d007b
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-04-12 21:01:44 -0700

    Compile a statically linked executable for faster startup speeds

--- release.nix
+++ release.nix
@@ -13,7 +13,9 @@ let
     packageOverrides = pkgs: {
       haskellPackages = pkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: {
-          dhall-bash = haskellPackagesNew.callPackage ./default.nix { };
+          dhall-bash =
+           pkgs.haskell.lib.disableSharedExecutables
+             (haskellPackagesNew.callPackage ./default.nix { });
         };
       };
     };
