feeed229cc9fd98f1ec58dd6c3a38e579378910e
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-10-15 22:12:50 -0700

    Version 1.0.15 → 1.0.16 (#12)

--- nix/dhall-bash.nix
+++ nix/dhall-bash.nix
@@ -1,16 +1,15 @@
 { mkDerivation, base, bytestring, containers, dhall
-, insert-ordered-containers, neat-interpolation, optparse-generic
-, shell-escape, stdenv, text
+, neat-interpolation, optparse-generic, shell-escape, stdenv, text
 }:
 mkDerivation {
   pname = "dhall-bash";
-  version = "1.0.15";
+  version = "1.0.16";
   src = ./..;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base bytestring containers dhall insert-ordered-containers
-    neat-interpolation shell-escape text
+    base bytestring containers dhall neat-interpolation shell-escape
+    text
   ];
   executableHaskellDepends = [
     base bytestring dhall optparse-generic text
